2d34aeeb3a517b7754d738c7f017a2fb2d7d82bd
Dien Nguyen <ndien9203@gmail.com>
2024-05-05 21:21:57 +0700

    design UI done

---
+++ src/main/java/vn/aptech/c2304l/learning/model/Category.java
@@ -0,0 +1,16 @@
+package vn.aptech.c2304l.learning.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category {
+    int id;
+    String name;
+}
