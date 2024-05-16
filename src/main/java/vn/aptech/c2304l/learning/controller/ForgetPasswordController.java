eec9089d375e073473a5aa26d3f841e279ee5fe9
Dien Nguyen <ndien9203@gmail.com>
2024-05-16 10:23:52 +0700

    update forgetPass and login

--- src/main/java/vn/aptech/c2304l/learning/controller/ForgetPasswordController.java
+++ src/main/java/vn/aptech/c2304l/learning/controller/ForgetPasswordController.java
@@ -1,29 +1,56 @@
 package vn.aptech.c2304l.learning.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import vn.aptech.c2304l.learning.Main;
+import vn.aptech.c2304l.learning.dal.UserDAO;
+import vn.aptech.c2304l.learning.utils.AlertNotification;
+import vn.aptech.c2304l.learning.utils.BcryptUtil;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ForgetPasswordController implements Initializable {
 
+    UserDAO userDAO = new UserDAO();
+    AlertNotification alert = new AlertNotification();
+    private BcryptUtil bcryptUtil = BcryptUtil.getInstance();
     @FXML
-    private Button btnBack;
+    private AnchorPane forgetPasswordPane;
 
     @FXML
     private Button btnCreateNewPassword;
 
     @FXML
-    private TextField username;
+    private Button btnConfirm;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private TextField txt_username;
+
+    @FXML
+    private AnchorPane confirmNewPasswordPane;
+
+    @FXML
+    private PasswordField newPassword;
+
+    @FXML
+    private PasswordField confirmNewPassword;
+
+    @FXML
+    private Button btnForgetPassword;
+
 
     @FXML
     public void redirectLogin() {
@@ -43,8 +70,53 @@ public class ForgetPasswordController implements Initializable {
         }
     }
 
+    private String username;
+    private String pass;
+    private String repass;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        btnCreateNewPassword.setOnAction(eh -> {
+            // Lấy giá trị từ trường txt_username khi người dùng nhấn nút
+            username = txt_username.getText().trim();
+
+            // Kiểm tra xem username có giá trị không trước khi sử dụng
+            if (!username.isEmpty()) {
+                if (userDAO.checkUsernameExists(username)) {
+                        alert.showAlert("Thông báo","Đã tìm thấy tài khoản");
+                        confirmNewPasswordPane.setVisible(true);
+                        forgetPasswordPane.setVisible(false);
+                } else {
+
+                    alert.showAlert("Lỗi", "Tài khoản '" + username + "' không tồn tại");
+                }
+            } else {
+                AlertNotification alert = new AlertNotification();
+                alert.showAlert("Lỗi", "Vui lòng nhập tên tài khoản");
+            }
+        });
+
+        btnForgetPassword.setOnAction(eh -> {
+            confirmNewPasswordPane.setVisible(false);
+            forgetPasswordPane.setVisible(true);
+        });
+
+
+
+        btnConfirm.setOnAction(eh -> {
+             pass = newPassword.getText().trim();
+             repass = confirmNewPassword.getText().trim();
+
+            if(pass.equals(repass)){
+                alert.showAlert("Thông báo","Đổi mật khẩu thành công");
+                String hashPassword = bcryptUtil.hashPassword(pass);
+                userDAO.changePassword(username,hashPassword);
+            }else{
+                alert.showAlert("Lỗi","Mật khẩu không khớp");
+            }
+        });
     }
 
 }
